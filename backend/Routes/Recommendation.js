659aa42f95e0bfceb893873d3d565cadef746e59
Himesh Shakya <himu.shakya122@gmail.com>
2026-02-22 23:26:57 +0545

    added routes for recommendation

--- backend/Routes/Recommendation.js
+++ backend/Routes/Recommendation.js
@@ -15,3 +15,13 @@ router.post("/",authenticate, async (req, res) => {
     res.status(400).json({ error: err.message });
   }
 });
+
+router.get("/",authenticate, async (req, res) => {
+  try {
+    const exercise = await Exercise.create(req.body);
+     res.status(200).json(exercise);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+export default router;
